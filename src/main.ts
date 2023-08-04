15f2948e494954d2f26da20869d1aeb70c5f4805
Kirill Lakhtachev <lahtachev@gmail.com>
2023-08-04 17:39:50 +0200

    small fixes

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue';
-import App from './components/App.vue';
-import routes from './routes';
+import App from '@/components/App.vue';
+import routes from '@/routes';
 import ui from '@/ui';
 
 createApp(App).use(ui).use(routes).mount('#app');
